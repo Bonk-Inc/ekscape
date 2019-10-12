709dc84c41123aa8e2439d33bd102d29855cafcb
jvdwijk <joeyvdwijk@gmail.com>
2019-10-12 13:13:14 +0200

    Grapple Start

---
+++ Assets/Scripts/Grapple.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Grapple : MonoBehaviour
+{
+    [SerializeField]
+    private float maxLength = 1;
+
+    [SerializeField]
+    private LineRenderer lineRenderer;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()){
+            //doe de roar!
+        }
+    }
+
+    private void ChangeLine(Vector3 destination)
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, destination);
+    }
+}
