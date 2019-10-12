58c0b3129e70f0571b358e7a981f35691e3a0b2f
Coen Steuns <c.steuns@gmail.com>
2019-10-12 16:24:56 +0200

    Player health

---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    private int health;
+
+    [SerializeField]
+    private int startHealth;
+
+    [SerializeField]
+    private int maxHealth = 3;
+
+
+    public int CurrentHealth => health;
+    public int StartHealth => startHealth;
+    public int MaxHealth => maxHealth;
+
+    public event Action<int> OnHealthChanged;
+
+    private void Awake()
+    {
+        health = startHealth;
+    }
+
+    public void SetStartHealth(int amount)
+    {
+        startHealth = amount;
+    }
+    public void SetMaxHealth(int amount)
+    {
+        maxHealth = amount;
+    }
+
+    public void AddHealth(int amount)
+    {
+        if (amount == 0)
+            return;
+
+        health += amount;
+        health = health > maxHealth ? maxHealth : health;
+        OnHealthChanged?.Invoke(health);
+    }
+
+}
