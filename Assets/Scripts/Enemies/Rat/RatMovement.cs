32e673e134d488647c48ae9900241af5a7e3c9b8
Coen Steuns <c.steuns@gmail.com>
2019-10-12 20:16:47 +0200

    Start rat

---
+++ Assets/Scripts/Enemies/Rat/RatMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float patrollSize = 2;
+
+    [SerializeField]
+    private float speed = 1;
+
+    private Rigidbody2D rb;
+    private float xOne, xTwo;
+    private float currentGoal;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        xOne = transform.position.x - patrollSize / 2;
+        xTwo = transform.position.x + patrollSize / 2;
+
+        currentGoal = xOne;
+    }
+
+    private void Update()
+    {
+        var dir = currentGoal - rb.transform.position.x;
+        rb.transform.position += 
+    }
+
+    private void OnDrawGizmos()
+    {
+        var pointOne = transform.position;
+        pointOne.x -= patrollSize / 2;
+
+        var pointTwo = transform.position;
+        pointTwo.x += patrollSize / 2;
+        Gizmos.DrawLine(pointOne, pointTwo);      
+    }
+
+}
