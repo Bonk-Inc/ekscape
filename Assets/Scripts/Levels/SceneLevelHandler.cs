0c86ac7f358eceff9349b1321e42c064fd528061
unknown <c.steuns@gmail.com>
2019-10-13 15:36:25 +0200

    Level 1

---
+++ Assets/Scripts/Levels/SceneLevelHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLevelHandler : MonoBehaviour
+{
+
+    private const string SceneLevelPrefix = "Play_Level_";
+
+    [SerializeField]
+    private int SceneLevel;
+
+    public string sceneName => GetSceneLevelName(SceneLevel);
+
+    public void LoadNextLevel()
+    {
+        if (!NextLevelExists())
+            return;
+
+        SceneManager.LoadScene(GetSceneLevelName(SceneLevel + 1));
+    }
+
+    private bool NextLevelExists()
+    {
+        var nextScene = SceneManager.GetSceneByName(GetSceneLevelName(SceneLevel + 1));
+        return nextScene != null;
+    }
+
+    private string GetSceneLevelName(int level)
+    {
+        return SceneLevelPrefix + level;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player") || !Input.GetKeyDown(KeyCode.W))
+            return;
+
+        LoadNextLevel();
+    }
+
+}
