38157094619db52ac787f3709e351b27d8249b14
unknown <c.steuns@gmail.com>
2019-10-13 16:22:52 +0200

    main menu

---
+++ Assets/Scripts/UI/QuitButtonHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitButtonHandler : MonoBehaviour
+{
+
+    [SerializeField]
+    private Button quitButton;
+
+
+
+    private void Awake()
+    {
+#if !UNITY_WEBGL && !UNITY_EDITOR
+        quitButton.onClick.AddListener(Quit);
+#else
+        quitButton.gameObject.SetActive(false);
+#endif
+    }
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
