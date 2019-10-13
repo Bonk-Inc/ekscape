e93a19fe5fd3084c66a635422a7dcf4088f037ee
unknown <c.steuns@gmail.com>
2019-10-13 22:14:19 +0200

    pew pew fix

---
+++ Assets/Scripts/Levels/WinscreenOpener.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinscreenOpener : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject winScreen;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player") || !Input.GetKeyDown(KeyCode.W))
+            return;
+
+
+        winScreen.SetActive(true);
+    }
+}
