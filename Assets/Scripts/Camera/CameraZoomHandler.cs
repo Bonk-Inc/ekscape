60291d390890a8d79b6b3235eefb7a8701946cc0
Coen Steuns <c.steuns@gmail.com>
2019-12-05 20:51:56 +0100

    added zoom camera calculation

---
+++ Assets/Scripts/Camera/CameraZoomHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoomHandler : MonoBehaviour
+{
+
+    [SerializeField]
+    private Collider2D level;
+
+    [SerializeField]
+    private Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetCameraPosition(level.bounds.center);
+        float size = 0;
+        if(level.bounds.size.x / level.bounds.size.y < camera.aspect)
+        {
+            size = level.bounds.size.y;
+        }
+        else
+        {
+            size = level.bounds.size.y / camera.aspect * (level.bounds.size.x / level.bounds.size.y);
+        }
+        camera.orthographicSize = size / 2;
+    }
+
+    private void SetCameraPosition(Vector2 pos)
+    {
+        var camPos = camera.transform.position; 
+        camPos.x = pos.x;
+        camPos.y = pos.y;
+        camera.transform.position = camPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
