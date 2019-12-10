dbda3c543e3a4bcd4abdeb2b88f83a23410c0e54
Coen Steuns <c.steuns@gmail.com>
2019-12-10 16:04:16 +0100

    All tutorials now in UI :)

--- Assets/Scripts/UI/gameplay/Magnifier/TriggerMagnifier.cs
+++ Assets/Scripts/UI/gameplay/Magnifier/TriggerMagnifier.cs
@@ -7,13 +7,22 @@ public class TriggerMagnifier : MonoBehaviour
     [SerializeField]
     private Magnifiable magnifiable;
 
+    [SerializeField]
+    private string acceptedTag = "Player";
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (!collision.CompareTag(acceptedTag))
+            return;
+
         Magnifier.Instance.MagnifyFadeIn(magnifiable);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        if (!collision.CompareTag(acceptedTag))
+            return;
+
         Magnifier.Instance.MagnifyFadeOut();
     }
 
