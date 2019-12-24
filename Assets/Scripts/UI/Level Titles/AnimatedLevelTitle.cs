ad45079926a3375ecb445b9fee60acb1bb12a338
Coen Steuns <c.steuns@gmail.com>
2019-12-24 21:10:05 +0100

    Title working and added to level 1

---
+++ Assets/Scripts/UI/Level Titles/AnimatedLevelTitle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedLevelTitle : LevelTitle
+{
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private string animationStateName = "Show", triggerName = "show";
+
+    protected override IEnumerator ShowLevelTitle(Action onFinished)
+    {
+        animator.SetTrigger(triggerName);
+        yield return new WaitUntilAnimationPlayed(animator, animationStateName);
+        onFinished?.Invoke();
+    }
+}
