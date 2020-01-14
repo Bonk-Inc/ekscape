043c336f5a97a1bb44768904a4b0f2a00b7b05e0
jvdwijk <joeyvdwijk@gmail.com>
2020-01-14 22:40:10 +0100

    landing effect

---
+++ Assets/Scripts/Utils/SelfDestruct.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+    [SerializeField]
+    private float destructTime = 1;
+
+    void Start()
+    {
+        StartCoroutine(DestructCoroutine());
+    }
+
+    private IEnumerator DestructCoroutine(){
+        yield return new WaitForSeconds(destructTime);
+        Destroy(gameObject);
+    }
+}
