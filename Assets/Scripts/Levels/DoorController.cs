7d72b67da654a6ca88be14d57a863fbd5ef15834
jvdwijk <joeyvdwijk@gmail.com>
2020-02-04 10:06:49 +0100

    new scene loading

---
+++ Assets/Scripts/Levels/DoorController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorController : MonoBehaviour
+{
+    private const string SceneLevelPrefix = "Play_Level_";
+
+    [SerializeField]
+    private RoomManager room;
+
+    [SerializeField]
+    private int roomDestination;
+
+    [SerializeField]
+    private bool isCheckpoint = true;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player") || !Input.GetKeyDown(KeyCode.W))
+            return;
+
+        room.SendToRoom(roomDestination);
+    }
+}
